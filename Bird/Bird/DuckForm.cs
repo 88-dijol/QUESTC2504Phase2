9fc34fa853b6a55c9cd24f0e8957165aafb66a37
DIJOL DAVIS <83902331+88-dijol@users.noreply.github.com>
2024-10-02 10:43:09 +0530

    25-09-25

---
+++ Bird/Bird/DuckForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bird
+{
+    public partial class DuckForm : Form
+    {
+        public DuckForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DuckForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
---
+++ Bird/Bird/MainPageForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bird
+{
+    public partial class MainPageForm : Form
+    {
+        public MainPageForm()
+        {
+            InitializeComponent();
+        }
+
+        private PeacockForm frmPeacock = new PeacockForm();
+        private DuckForm frmDuck = new DuckForm();
+        private CrowForm frmCrow = new CrowForm();
+        private void mnuPeacock_Click(object sender, EventArgs e)
+        {
+            frmPeacock.MdiParent = this;
+            frmPeacock.Show();
+            frmPeacock.Activate();
+
+        }
+
+        private void mnuDuck_Click(object sender, EventArgs e)
+        {
+            frmDuck.MdiParent = this;
+            frmDuck.Show();
+            frmDuck.Activate();
+
+        }
+
+        private void mnuCrow_Click(object sender, EventArgs e)
+        {
+            frmCrow.MdiParent = this;
+            frmCrow.Show();
+            frmCrow.Activate();
+
+        }
+    }
+}
