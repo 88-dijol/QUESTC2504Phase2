a778157f2296a9e7a6f2739ee751800f122f21ce
DIJOL DAVIS <83902331+88-dijol@users.noreply.github.com>
2024-10-02 10:34:20 +0530

    1-Tuesday MVVM

---
+++ 1-Tue/WPF1STSolution/FullNameProject/DClose.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullNameProject
+{
+    public delegate void DClose();
+    
+}
