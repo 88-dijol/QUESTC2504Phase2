873508db1e3a0b2323da42d894a741719933c6a8
DIJOL DAVIS <83902331+88-dijol@users.noreply.github.com>
2024-10-06 23:44:01 +0530

    bank project

---
+++ BankingProjectTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankingProjectTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("QuEST-Global")]
+[assembly: AssemblyProduct("BankingProjectTest")]
+[assembly: AssemblyCopyright("Copyright © QuEST-Global 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a37a0bb-d235-4694-adef-7479ec2c20f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
